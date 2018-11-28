d597897ae5e79290a5e04e1f892234821e4cc092
RdFiction <vkrassilnikova@gmail.com>
2018-11-28 07:35:46 +0400

    HW Lesson2 Правки

--- Gig.PensionCalc.Terminal/Program.cs
+++ Gig.PensionCalc.Terminal/Program.cs
@@ -17,35 +17,13 @@ namespace Gig.PensionCalc.Terminal
 
             #region MyRegion
 
-
-            //var man = new Userinfo();
-            //user.Name = name;
-            //user.Sex = isWoman ? Sex.Woman : Sex.Man;
-            //user.BirthDay = DateTime.Today.AddYears(-age);
-
-            // var calc = new Calculator();
-            // var pensioninfo = calc.Calc(user);
-
-            #endregion
-
-            //var tt = new TimeSpan(23, 2, 56, 34);
-
-            //var result = $"{user.Name}, тебе работать еще примерно {pensioninfo.RemainingYears.TotalDays} дней";
-
-
-            // Console.WriteLine(result);
-
-            //var now = 2018;
-
             var user = new UserInfo();
 
             Console.Write("Напишите ФИО: ");
             user.Name = Console.ReadLine();
 
             Console.Write("Укажите пол (М/Ж): ");
-            string sex = Console.ReadLine();
-            //bool sex1 = sex.ToUpper() == "М";
-            user.Sex = Sex.Unknown;
+            string sex = Console.ReadLine();            user.Sex = Sex.Unknown;
             if (sex.ToUpper() == "M" || sex.ToUpper() == "М")
                 user.Sex = Sex.Man;
             if (sex.ToUpper() == "Ж")
@@ -61,6 +39,9 @@ namespace Gig.PensionCalc.Terminal
             Console.Write("Вы - {0} ({1}), родились в {2} году, вам {3} лет, \nВыход на пенсию в {4} году, Вам осталось работать {5} лет.", user.Name, user.Sex, user.BirthDay.ToShortDateString(), Math.Round(user.Age.TotalDays/365), pensionInfo.PensionDate.ToShortDateString(), Math.Round(pensionInfo.RemainingYears.TotalDays/365));
             
             Console.ReadKey();
+
+            #endregion
+
         }
     }
 }
